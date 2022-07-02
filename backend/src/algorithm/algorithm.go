7fedda7f08748786caaacea967b410e870c5654d
BitlyTwiser <joshua.groeschl@tutanota.com>
2022-07-02 09:02:44 -0700

    Closer!

--- backend/src/algorithm/algorithm.go
+++ backend/src/algorithm/algorithm.go
@@ -5,7 +5,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"math"
-	"math/cmplx"
 	"net/http"
 )
 
@@ -35,13 +34,15 @@ func GetFutureCostPrediction(w http.ResponseWriter, req *http.Request) {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
+	// Python algo example.
+	// probas.append((1/np.sqrt(2*np.pi*self.stds_[j]**2)*np.exp(-0.5*((X[i]-self.means_[j])/self.stds_[j])**2)).prod()*self.priors_[j])
+	// Get mean of dataset and standard deviation for equation.
 
 	fmt.Printf("Starting cost prediction analysis for token %s", r.CoinId)
-	fmt.Println(math.E)
-	fmt.Println(math.Pi)
-	fmt.Println(math.Sqrt(8))
-	fmt.Printf("More Complex %v", cmplx.Sqrt(-5*12i))
-	fmt.Printf("Exponent: %.1f", math.Pow(6, 2))
+	// fmt.Println(math.E)
+	// fmt.Println(math.Pi)
+	// fmt.Println(math.Sqrt(8))
+	// fmt.Printf("More Complex %v", cmplx.Sqrt(-5*12i))
 
 	a := algorithm{
 		token:              r.CoinId,
@@ -50,12 +51,14 @@ func GetFutureCostPrediction(w http.ResponseWriter, req *http.Request) {
 		currentTokenPrice:  r.Price,
 	}
 
-	fmt.Println("Now Printing flat values")
-	for _, v := range a.classificationData {
-		fmt.Println(v)
-	}
-
-	a.train()
+	// fmt.Println("Now Printing flat values")
+	// for _, v := range a.classificationData {
+	// 	fmt.Println(v)
+	// }
+	fmt.Printf("Exponent: %.1f\n", math.Pow(6, 2))
+	fmt.Printf("Mean: %v\n", a.mean())
+	fmt.Printf("Standard Deviation: %v\n", a.standardDeviation())
+	fmt.Printf("Probability: %v", a.naiveGuassianBayesAlgorithm())
 	// fmt.Println(a.classificationData)
 	// At the end take the first and last price, calculate different, add that to price or subtract depending on if the price is going to move up or down.
 }
@@ -81,6 +84,41 @@ func cleanClassificationData(classificationData coinGecko.OhlcData) []float64 {
 		}
 	}
 	return flatValues
+}
+
+func (alg *algorithm) mean() float64 {
+	sum := alg.sumArray(alg.classificationData...)
+
+	return math.Ceil((sum / float64(len(alg.classificationData))))
+}
+
+func (alg *algorithm) standardDeviation() float64 {
+	// Gather mean
+	mean := alg.mean()
+
+	// setting (val - mean) ^ 2 to all elements of array
+	for i, val := range alg.classificationData {
+		alg.classificationData[i] = math.Pow((val - mean), 2)
+	}
+
+	varaiance := alg.sumArray(alg.classificationData...)
+
+	return math.Sqrt(varaiance)
+}
+
+// P(x_i \mid y) = \frac{1}{\sqrt{2\pi\sigma^2_y}} \exp\left(-\frac{(x_i - \mu_y)^2}{2\sigma^2_y}\right)
+// I think we loop here, build probabbilitis, used trained data to determine the probvabilty of our current price ,then return value
+func (alg *algorithm) naiveGuassianBayesAlgorithm() float64 {
+	initial := (1 / (math.Sqrt(2 * math.Pi * math.Pow(alg.standardDeviation(), 2))))
+	second := (math.Pow(alg.eulersConstant, math.Pow(-0.5*(alg.currentTokenPrice-(alg.mean()/alg.standardDeviation())), 2)))
+
+	probability := initial * second
+
+	return probability
+}
+
+func (alg *algorithm) priceCalculation() {
+
 }
 
 func (alg *algorithm) dataClassigicationEngine() {
