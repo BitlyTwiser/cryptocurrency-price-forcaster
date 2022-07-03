8ea55a4ff5feace9339759d06d11778a05ca21b3
BitlyTwiser <joshua.groeschl@tutanota.com>
2022-07-03 12:41:09 -0700

    Killing things

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+    <p><small>Photo courtesy of <a href="https://unsplash.com/">Unsplash</a></small></p>
     <landing-page />
   </div>
 </template>
